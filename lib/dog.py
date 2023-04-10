83bb9f43a40de463bef9c8fcd3d5cef6f83053f5
Oliver <olichase91@gmail.com>
2023-04-10 11:44:04 -0700

    Solved Lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = 'Mutt'):
+        self.name = name
+        self.breed = breed
+
+    def bark(self):
+        print('Woof!')
+
+    def show_self(self):
+        return self
+    
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+
+snoopy = Dog('Snoopy', 'Beagle')
+snoopy.bark()
+
+print(snoopy is snoopy.show_self())
+
+print(snoopy.name)
+
+snoopy.owner = 'Charlie Brown'
+
+print(snoopy.owner)
+
+snoopy.get_adopted('Linus')
+
+print(snoopy.owner)
+
+print(snoopy.breed)
+
